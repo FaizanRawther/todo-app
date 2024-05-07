c68c4af59f97f75835d599b7809a316706aae7de
faizan <faizanrawther2001@gmail.com>
2024-05-07 22:48:56 +0530

    Create basic GUI without basic interaction. Day16

--- GraphicalUserInterface.py
+++ GraphicalUserInterface.py
@@ -1,9 +1,19 @@
 import FreeSimpleGUI as fsg
 import functions as func
 label=fsg.Text("Type in to-do")
-input_box=fsg.InputText(tooltip="Enter Todo")
+input_box=fsg.InputText(tooltip="Enter Todo",key='todo')
 add_button=fsg.Button("Add")
-exit=fsg.qu
-window=fsg.Window("My Todo App",layout=[[[label],[input_box,add_button],[exit]]])
-window.read()
+
+window=fsg.Window("My Todo App",
+                  layout=[[[label],
+                           [input_box,add_button]]],
+                  font=('helvetica',14))
+while True:
+    event,values=window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos=func.read_todos()
+
 window.close()
